fefdab9a6b8ec462884237bc3c9d07e03bdb8573
MrFiona <1160177283@qq.com>
2017-08-31 22:31:50 +0800

    bisect、装饰器总结

--- decorator_wrap.py
+++ decorator_wrap.py
@@ -4,3 +4,15 @@
 # Author  : MrFiona
 # File    : decorator_wrap.py
 # Software: PyCharm
+
+
+#todo 装饰器总结
+
+# todo 装饰器是一个很著名的设计模式，经常被用于有切面需求的场景，较为经典的有插入日志、性能测试、事务处理等。
+# todo 装饰器是解决这类问题的绝佳设计，有了装饰器，我们就可以抽离出大量函数中与函数功能本身无关的雷同代码并继续重用。
+# todo 概括的讲，装饰器的作用就是为已经存在的对象添加额外的功能
+
+
+from functools import wraps
+
+
--- summmary_bisect.py
+++ summmary_bisect.py
@@ -11,7 +11,12 @@ import bisect
 
 test_bisect = sorted(['cat', 'dog', 'pig', 'mouse', 'tiger', 'sheep', 'chicken'])
 print test_bisect
-print bisect.bisect(test_bisect, 'looser')
-# print test_bisect
-print bisect.bisect_left(test_bisect, 'peqwe')
+print 'default bisect:', bisect.bisect(test_bisect, 'default bisect')
+print 'right bisect:', bisect.bisect_right(test_bisect, 'right bisect')
+print 'left bisect:', bisect.bisect_left(test_bisect, 'left bisect')
+bisect.insort(test_bisect, 'right')
+print test_bisect
+bisect.insort_right(test_bisect, 'insort right')
+print test_bisect
+bisect.insort_left(test_bisect, 'insort left')
 print test_bisect
