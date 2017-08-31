766522e491ac743e91a80fc3ef11896c2f1c7b0b
MrFiona <1160177283@qq.com>
2017-09-01 01:23:50 +0800

    更新装饰器总结

--- decorator_wrap.py
+++ decorator_wrap.py
@@ -16,3 +16,229 @@
 from functools import wraps
 
 
+# todo ********************************** 1、最简单的装饰函数的装饰器 **********************************
+def decorator_func(func):
+    def wrapper():
+        print 'before func() called.'
+        func()
+        #todo 名称变成了wrapper
+        print wrapper.__name__
+        print 'after func() called.'
+    return wrapper
+
+@decorator_func
+def test_func():
+    a = 1
+    print 'this is func.'
+# print '\033[31m1、最简单的装饰函数的装饰器\033[0m'
+# print '\033[32m1、最简单的装饰函数的装饰器\033[0m'
+# print '\033[33m1、最简单的装饰函数的装饰器\033[0m'
+# print '\033[34m1、最简单的装饰函数的装饰器\033[0m'
+# print '\033[35m1、最简单的装饰函数的装饰器\033[0m'
+# print '\033[36m1、最简单的装饰函数的装饰器\033[0m'
+# print '\033[37m1、最简单的装饰函数的装饰器\033[0m'
+
+print '\033[31m1、最简单的装饰函数的装饰器\033[0m'
+test_func()
+print '\033[31m1、最简单的装饰函数的装饰器\033[0m'
+# todo ********************************** 1、最简单的装饰函数的装饰器 **********************************
+
+
+
+# todo ***************************** 2、保持原有被装饰函数特性不变的装饰器 *****************************
+def decorator_func(func):
+    @wraps(func)
+    def wrapper():
+        print 'before func() called.'
+        func()
+        #todo 名称依然为test_func
+        print wrapper.__name__
+        print 'after func() called.'
+    return wrapper
+
+@decorator_func
+def test_func():
+    a = 1
+    print 'this is func.'
+
+print '\n\033[32m2、保持原有被装饰函数特性不变的装饰器\033[0m'
+test_func()
+print '\033[32m2、保持原有被装饰函数特性不变的装饰器\033[0m'
+# todo ***************************** 2、保持原有被装饰函数特性不变的装饰器 *****************************
+
+
+
+# todo ********************************* 3、装饰带有参数的函数的装饰器 *********************************
+def decorator_func(func):
+    @wraps(func)
+    def wrapper(a, b):
+        print 'before func() called.'
+        func(a, b)
+        print 'this is wraps:\ta = {}\tb = {}' .format(a, b)
+        print 'after func() called.'
+    return wrapper
+
+@decorator_func
+def test_func(a, b):
+    print 'this is func:\ta = {}\tb = {}' .format(a, b)
+
+print '\n\033[36m3、装饰带有参数的函数的装饰器\033[0m'
+test_func(a='hello', b='world')
+print '\033[36m3、装饰带有参数的函数的装饰器\033[0m'
+# todo ********************************* 3、装饰带有参数的函数的装饰器 *********************************
+
+
+
+# todo ****************************** 4、装饰带有不固定参数的函数的装饰器 ******************************
+def decorator_func(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print 'before func() called.'
+        # todo 不要写成 func(args, kwargs)这样就变成可两个固定参数
+        func(*args, **kwargs)
+        print 'this is wraps:\targs = {}\tkwargs = {}' .format(args, kwargs)
+        print 'after func() called.'
+    return wrapper
+
+@decorator_func
+def test_func(*args, **kwargs):
+    print 'this is func:\targs = {}\tkwargs = {}' .format(args, kwargs)
+
+print '\n\033[35m4、装饰带有不固定参数的函数的装饰器\033[0m'
+test_func(100, 'decorator', a='hello', b='world')
+print '\033[35m4、装饰带有不固定参数的函数的装饰器\033[0m'
+# todo ****************************** 4、装饰带有不固定参数的函数的装饰器 ******************************
+
+
+
+# todo *********************************** 5、装饰器带有参数的装饰器 ***********************************
+def out_decorator(arg):
+    def decorator_func(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            print 'before func() called.'
+            # todo 不要写成 func(args, kwargs)这样就变成可两个固定参数
+            func(*args, **kwargs)
+            print 'this is wraps 1:\targs = {}\tkwargs = {}\targ = {}' .format(args, kwargs, arg)
+            print 'this is wraps 2:\targs = {}\tkwargs = {}\targ = {}' .format(args, kwargs, arg)
+            print 'after func() called.'
+        return wrapper
+    return decorator_func
+
+@out_decorator('test_decorator_parameter')
+def test_func(*args, **kwargs):
+    print 'this is func:\targs = {}\tkwargs = {}' .format(args, kwargs)
+
+print '\n\033[33m5、装饰器带有参数的装饰器\033[0m'
+test_func(100, 'decorator', a='hello', b='world')
+print '\033[33m5、装饰器带有参数的装饰器\033[0m'
+# todo ************************************** 5、带有参数的装饰器 **************************************
+
+
+
+# todo *********************************** 6、带有类方法参数的装饰器 ***********************************
+class TestLock:
+    #todo staticmethod和classmethod作为静态和类装饰器是类的方法,可以直接通过类而不通过实力进行调用
+    def __init__(self):
+        print 'TestLock.__init__ is called.'
+
+    @staticmethod
+    def  acquire():
+        print 'TestLock.acquire is called. This is a static method(这是静态方法, 不需要对象实例).'
+
+    @classmethod
+    def release(cls):
+        print 'TestLock.release is called. This is a class method(这是类方法, 不需要对象实例).'
+
+
+def out_decorator(cls):
+    def decorator_func(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            print 'before func() called.'
+            cls.acquire()
+            try:
+                # todo 不要写成 func(args, kwargs)这样就变成可两个固定参数
+                func(*args, **kwargs)
+                print 'this is wraps 1:\targs = {}\tkwargs = {}\targ = {}' .format(args, kwargs, cls)
+                print 'this is wraps 2:\targs = {}\tkwargs = {}\targ = {}' .format(args, kwargs, cls)
+                print 'after func() called.'
+            finally:
+                cls.release()
+        return wrapper
+    return decorator_func
+
+@out_decorator(TestLock)
+def test_func(*args, **kwargs):
+    print 'this is func:\targs = {}\tkwargs = {}' .format(args, kwargs)
+
+print '\n\033[32m6、装饰器带有参数的装饰器\033[0m'
+test_func(100, 'decorator', a='hello', b='world')
+print '\033[32m6、装饰器带有参数的装饰器\033[0m'
+# todo *********************************** 6、带有类方法参数的装饰器 ***********************************
+
+
+
+# todo ********************* 7、带有类方法参数的装饰器并对同一个函数应用多个装饰器 **********************
+class mylocker:
+    def __init__(self):
+        print("mylocker.__init__() called.")
+
+    @staticmethod
+    def acquire():
+        print("mylocker.acquire() called.")
+
+    @staticmethod
+    def unlock():
+        print("mylocker.unlock() called.")
+
+
+class lockerex(mylocker):
+    @staticmethod
+    # def acquire():
+    #     print("lockerex.acquire() called.")
+
+    @staticmethod
+    def unlock():
+        print("lockerex.unlock() called.")
+
+
+def out_decorator(cls):
+    def decorator_func(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            print 'before func() called.'
+            cls.acquire()
+            try:
+                # todo 不要写成 func(args, kwargs)这样就变成可两个固定参数
+                print 'this is wraps 1:\targs = {}\tkwargs = {}\targ = {}'.format(args, kwargs, cls)
+                print 'this is wraps 2:\targs = {}\tkwargs = {}\targ = {}'.format(args, kwargs, cls)
+                print 'after func() called.'
+                return func(*args, **kwargs)
+            finally:
+                cls.unlock()
+        return wrapper
+    return decorator_func
+
+
+class example:
+    @out_decorator(mylocker)
+    def myfunc(self, a, b):
+        print(" myfunc() called.")
+        return a + b
+
+    @out_decorator(mylocker)
+    @out_decorator(lockerex)
+    def myfunc2(self, a, b):
+        print(" myfunc2() called.")
+        return a + b
+
+
+print '\n\033[31m7、装饰器带有参数的装饰器\033[0m'
+a = example()
+a.myfunc('200 + ', 'string parameter')
+print(a.myfunc('3000 + ', 'hello'))
+print(a.myfunc2(1, 2))
+print(a.myfunc2(3, 4))
+print '\033[31m7、装饰器带有参数的装饰器\033[0m'
+# todo ********************* 7、带有类方法参数的装饰器并对同一个函数应用多个装饰器 **********************
