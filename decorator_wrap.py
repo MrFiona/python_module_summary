b07b28c47534cfc45bce3c3405c1bd2f80b710eb
MrFiona <1160177283@qq.com>
2017-09-01 18:47:25 +0800

    更新装饰器总结

--- decorator_wrap.py
+++ decorator_wrap.py
@@ -266,7 +266,7 @@ def log(obj):
             print 'kwargs: ', kw
             print 'obj:', obj, '\t', obj.__name__, '\t', type(obj)
             print 'end2 %s():' % (func.__name__)
-        return wrappertr
+        return wrapper
 
 
 @log
