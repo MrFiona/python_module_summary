06af126e9cf19e66ade8ef5eab3664e9452552ff
MrFiona <1160177283@qq.com>
2017-10-01 15:10:01 +0800

    增加时间模块time总结

---
+++ summary_time.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Time    : 2017-10-01 05:18
+# Author  : MrFiona
+# File    : summary_time.py
+# Software: PyCharm Community Edition
+
+
+
+import time
+
+print '\033[35m******************************************************************************************************\033[0m'
+print '\033[36mtime的接口如下所示:\033[0m\t'
+print
+print '\033[31m将当前的时间转化为自epoch时间起点的秒:\ttime.time():\t\033[0m', time.time()
+print '\033[31m将当前的时间转化为自epoch时间起点的元祖:\ttime.gmtime(0):\t\033[0m', time.gmtime(0)
+print
+
+print '\033[34m将当前时间的小数秒转化为当前的UTC时间的字符串表示:\ttime.asctime(time.gmtime(time.time())):\t\033[0m', time.asctime(time.gmtime(time.time()))
+print "\033[34m将当前时间的小数秒转化为当前的UTC时间的字符串表示:\ttime.strftime('%Y-%m-%d %H-%M-%S', time.gmtime(time.time())):\t\033[0m", time.strftime('%Y-%m-%d %H-%M-%S', time.gmtime(time.time()))
+print
+
+print '\033[34m将当前时间的小数秒转化为当前的epoch时间的字符串表示:\ttime.asctime(time.localtime(time.time())):\t\033[0m', time.asctime(time.localtime(time.time()))
+print "\033[34m将当前时间的小数秒转化为当前的epoch时间的字符串表示:\ttime.strftime('%Y-%m-%d %H-%M-%S', time.localtime(time.time())):\t\033[0m", time.strftime('%Y-%m-%d %H-%M-%S', time.localtime(time.time()))
+print
+
+print "\033[31m将epoch时间起点的小数秒转化为UTC时间的字符串表示:\ttime.asctime(time.gmtime(0)):\t\033[0m\t", time.asctime(time.gmtime(0))
+print "\033[31m将epoch时间起点的小数秒转化为UTC时间的字符串表示:\ttime.strftime('%Y-%m-%d %H-%M-%S', time.gmtime(0)):\t\033[0m\t", time.strftime('%Y-%m-%d %H-%M-%S', time.gmtime(0))
+print
+
+print '\033[32mepoch时间起点的元祖表示:\ttime.localtime(time.time()):\t\033[0m\t', time.localtime(time.time())
+print time.mktime(time.localtime(time.time()))
+print '\033[35m******************************************************************************************************\033[0m\n'
+
+
+
+
+# epoch_tuple_time = time.gmtime(0)
+# print '\033[31m获取时间起点epoch时间元祖 time.gmtime(0):\033[0m\t', epoch_tuple_time
+# g = time.gmtime(time.time())
+# print '\033[32m获取自epoch时间起点算起的当前的时间 time.gmtime(time.time()):\033[0m\t', g
+# local_tuple_time = time.localtime(time.time())
+# print epoch_tuple_time
+# print time.asctime(epoch_tuple_time)
+# print time.asctime(g)
+# print time.asctime(local_tuple_time)
