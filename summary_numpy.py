5419ab4c18f5974f8e79bfcce7df3d1bd3b85c75
MrFiona <1160177283@qq.com>
2017-10-14 00:06:53 +0800

    修改summary_numpy.py文件

--- summary_numpy.py
+++ summary_numpy.py
@@ -13,51 +13,51 @@ import numpy as np
 #todo reshape行数列数乘积需要保证等于6
 array_1 =  np.arange(6)
 x = array_1.reshape((2,3))
-print 'x:\t', x
+print '\033[36mx:\033[0m', x
 
 #todo arange类似于python的range函数
 array_2 = np.arange(1, 10, 2, dtype=np.float)
-print 'array_2:\t', array_2
+print '\033[36marray_2:\033[0m', array_2
 
-print '维度数量：', x.ndim
-print '数组的形状：', x.shape
-print '数组的元素类型：', x.dtype
-print '数组的元素数量：', x.size
+print '\033[32m维度数量：\033[0m', x.ndim
+print '\033[32m数组的形状：\033[0m', x.shape
+print '\033[32m数组的元素类型：\033[0m', x.dtype
+print '\033[32m数组的元素数量：\033[0m', x.size
 
 array_3 = np.array(range(4, 20, 2), dtype=np.int)
 array_4 = np.array([[1,2,324], [23,45,76]])
-print 'array_3:\t', array_3
-print 'array_4:\t', array_4
+print '\033[32marray_3:\033[0m', array_3
+print '\033[32marray_4:\033[0m', array_4
 
 array_one = np.ones(3, dtype=np.int)
 array_one_1 = np.ones((3,4), dtype=np.int)
-print 'array_one:\t', array_one
-print 'array_one_1:\t', array_one_1
+print '\033[32marray_one:\033[0m', array_one
+print '\033[32marray_one_1:\033[0m', array_one_1
 
 array_empty = np.empty(3)
 array_empty_1 = np.empty((3,4), dtype=np.int)
 #todo 不指定数据类型则默认是float类型,数据则是浮点型随机数据
 array_empty_2 = np.empty((3,4))
-print 'array_empty:\t', array_empty
-print 'array_empty_1:\t', array_empty_1
-print 'array_empty_2:\t', array_empty_2
+print '\033[35marray_empty:\033[0m', array_empty
+print '\033[35marray_empty_1:\033[0m', array_empty_1
+print '\033[35marray_empty_2:\033[0m', array_empty_2
 
 array_zeros = np.zeros(3)
 array_zeros_1 = np.zeros((3,4), dtype=np.int)
 #todo 不指定数据类型则默认是float类型
 array_zeros_2 = np.zeros((3,4))
-print 'array_zeros:\t', array_zeros
-print 'array_zeros_1:\t', array_zeros_1
-print 'array_zeros_2:\t', array_zeros_2
+print '\033[35marray_zeros:\033[0m', array_zeros
+print '\033[35marray_zeros_1:\033[0m', array_zeros_1
+print '\033[35marray_zeros_2:\033[0m', array_zeros_2
 
 #todo 改变已有数组的数据类型
-print x.dtype
+print '\033[32mx.dtype:\033[0m', x.dtype
 change_x = x.astype(np.float)
-print change_x.dtype
+print '\033[32mchange_x.dtype:\033[0m', change_x.dtype
 
 #todo reshape修改数组形状，-1代表当前维度的元素个数，自动计算
 reshape_array_3 = array_3.reshape(4,-1)
-print 'reshape_array_3:\t', reshape_array_3
+print '\033[32mreshape_array_3:\033[0m', reshape_array_3
 
 b = np.array([[0, 1], [2, 3]])
 m = np.array([[0, 1], [2, 3]])
