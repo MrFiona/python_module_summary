e58beff7298fdc0916f40dbf8ef25f41130ea338
MrFiona <1160177283@qq.com>
2017-10-13 23:10:56 +0800

    修改summary_numpy.py文件

--- summary_numpy.py
+++ summary_numpy.py
@@ -22,3 +22,13 @@ print '维度数量：', x.ndim
 print '数组的形状：', x.shape
 print '数组的元素类型：', x.dtype
 print '数组的元素数量：', x.size
+
+array_3 = np.array(range(4, 20, 2), dtype=np.int)
+array_4 = np.array([[1,2,324], [23,45,76]])
+print array_3
+print array_4
+
+array_one = np.ones(3, dtype=np.int)
+array_one_1 = np.ones((3,4), dtype=np.int)
+print array_one
+print array_one_1
