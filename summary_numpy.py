a2be0454453734e2d8aaed7d82ee564b829616d5
MrFiona <1160177283@qq.com>
2017-10-13 23:59:41 +0800

    修改summary_numpy.py文件

--- summary_numpy.py
+++ summary_numpy.py
@@ -9,14 +9,15 @@
 
 import numpy as np
 
+
 #todo reshape行数列数乘积需要保证等于6
 array_1 =  np.arange(6)
 x = array_1.reshape((2,3))
-print x
+print 'x:\t', x
 
 #todo arange类似于python的range函数
 array_2 = np.arange(1, 10, 2, dtype=np.float)
-print array_2
+print 'array_2:\t', array_2
 
 print '维度数量：', x.ndim
 print '数组的形状：', x.shape
@@ -25,29 +26,29 @@ print '数组的元素数量：', x.size
 
 array_3 = np.array(range(4, 20, 2), dtype=np.int)
 array_4 = np.array([[1,2,324], [23,45,76]])
-print array_3
-print array_4
+print 'array_3:\t', array_3
+print 'array_4:\t', array_4
 
 array_one = np.ones(3, dtype=np.int)
 array_one_1 = np.ones((3,4), dtype=np.int)
-print array_one
-print array_one_1
+print 'array_one:\t', array_one
+print 'array_one_1:\t', array_one_1
 
 array_empty = np.empty(3)
 array_empty_1 = np.empty((3,4), dtype=np.int)
 #todo 不指定数据类型则默认是float类型,数据则是浮点型随机数据
 array_empty_2 = np.empty((3,4))
-print array_empty
-print array_empty_1
-print array_empty_2
+print 'array_empty:\t', array_empty
+print 'array_empty_1:\t', array_empty_1
+print 'array_empty_2:\t', array_empty_2
 
 array_zeros = np.zeros(3)
 array_zeros_1 = np.zeros((3,4), dtype=np.int)
 #todo 不指定数据类型则默认是float类型
 array_zeros_2 = np.zeros((3,4))
-print array_zeros
-print array_zeros_1
-print array_zeros_2
+print 'array_zeros:\t', array_zeros
+print 'array_zeros_1:\t', array_zeros_1
+print 'array_zeros_2:\t', array_zeros_2
 
 #todo 改变已有数组的数据类型
 print x.dtype
@@ -56,24 +57,24 @@ print change_x.dtype
 
 #todo reshape修改数组形状，-1代表当前维度的元素个数，自动计算
 reshape_array_3 = array_3.reshape(4,-1)
-print reshape_array_3
+print 'reshape_array_3:\t', reshape_array_3
 
 b = np.array([[0, 1], [2, 3]])
 m = np.array([[0, 1], [2, 3]])
 b.resize((2,1))
 m.resize((2,3))
-print b
-print m
+print '\033[36mb:\033[0m', b
+print '\033[36mm:\033[0m', m
 
 #todo 矩阵的乘积 其中一个矩阵的行等于另一个矩阵的列
 m = np.arange(1, 12, 2)
 m = m.reshape((2,3))
-print 'm:', m
+print '\033[36mm:\033[0m', m
 
 n = np.arange(2, 14, 2, dtype=np.float)
 n = n.reshape((3,2))
-print 'n:', n
-print 'np.dot(m, n):', np.dot(m, n)
+print '\033[36mn:\033[0m', n
+print '\033[36mnp.dot(m, n):\033[0m', np.dot(m, n)
 
 #todo ndarray-多维数组的索引
 test_array = np.array([[[2, 3, 4, 5],
@@ -84,14 +85,14 @@ test_array = np.array([[[2, 3, 4, 5],
                         [2, 5, 6, 8]],
                        [[2, 3, 6, 8],
                         [3, 4, 8, 9]]])
-print 'test_array[1]:', test_array[1]
-print 'test_array[1][0]:', test_array[1][0]
-print 'test_array[1][0][1:3]:', test_array[1][0][1:3]
-print 'test_array[1, :, 1:3]:', test_array[1, :, 1:3]
-print 'test_array[1, :][1:]:', test_array[1, :][1:]
-print 'test_array[1, :][0]:', test_array[1, :][0]
-print 'test_array[1, :]:', test_array[1, :]
-print 'test_array[1][:]:', test_array[1][:]
+print '\033[32mtest_array[1]:\033[0m', test_array[1]
+print '\033[32mtest_array[1][0]:\033[0m', test_array[1][0]
+print '\033[32mtest_array[1][0][1:3]:\033[0m', test_array[1][0][1:3]
+print '\033[32mtest_array[1, :, 1:3]:\033[0m', test_array[1, :, 1:3]
+print '\033[32mtest_array[1, :][1:]:\033[0m', test_array[1, :][1:]
+print '\033[32mtest_array[1, :][0]:\033[0m', test_array[1, :][0]
+print '\033[32mtest_array[1, :]:\033[0m', test_array[1, :]
+print '\033[32mtest_array[1][:]:\033[0m', test_array[1][:]
 
 #todo ndarray-布尔类型索引
 #todo numpy中不能使用过python的and、or、not操作，使用逻辑与(&)，逻辑或(|)，逻辑非(~)来替换
@@ -101,14 +102,14 @@ scores = np.array([[94, 78, 91, 67],
                    [80, 79, 82, 92],
                    [100, 59, 93, 95]])
 classes = np.array([u'语文', u'英语', u'物理', u'数学'])
-print scores[names == 'Jeff']
-print scores[names == 'Jeff'].reshape((-1,))[classes == u'物理']
-print scores[(names == 'Jeff') | (names == 'Lily')]
-print scores[(names != 'Jeff') & (names != 'Lily') & (names != 'John')]
+print "\033[35mscores[names == 'Jeff']:\t\033[0m", scores[names == 'Jeff']
+print "\033[35mscores[names == 'Jeff'].reshape((-1,))[classes == u'物理']:\t\033[0m", scores[names == 'Jeff'].reshape((-1,))[classes == u'物理']
+print "\033[35mscores[(names == 'Jeff') | (names == 'Lily')]:\t\033[0m", scores[(names == 'Jeff') | (names == 'Lily')]
+print "\033[35mscores[(names != 'Jeff') & (names != 'Lily') & (names != 'John')]:\t\033[0m", scores[(names != 'Jeff') & (names != 'Lily') & (names != 'John')]
 
 #todo ndarray-花式索引
 array = np.arange(32).reshape(8, 4)
-print array, type(array)
+print 'array:\t', array, type(array)
 #todo 获取第0、3、5行的数据
 print '获取第0、3、5行的数据:\n', array[[0, 3, 5]]
 #todo 获取第(0, 0)，(3, 3)，(5, 3)这三个索引位置的数据
