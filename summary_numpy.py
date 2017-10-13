861518cd6cb6d5d647822d2251aa97b6517c0a35
MrFiona <1160177283@qq.com>
2017-10-13 23:04:37 +0800

    增加summary_numpy.py文件

---
+++ summary_numpy.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Time    : 2017-10-13 22:59
+# Author  : MrFiona
+# File    : summary_numpy.py
+# Software: PyCharm Community Edition
+
+
+
+import numpy as np
+
+#todo reshape行数列数乘积需要保证等于6
+array_1 =  np.arange(6)
+x = array_1.reshape((2,3))
+print x
