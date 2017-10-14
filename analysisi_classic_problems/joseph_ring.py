8270920f85975017865be269e0355ae6d3662647
MrFiona <1160177283@qq.com>
2017-10-14 19:16:08 +0800

    增加analysisi_classic_problems目录并增加文件joseph_ring.py

--- analysisi_classic_problems/joseph_ring.py
+++ analysisi_classic_problems/joseph_ring.py
@@ -4,3 +4,32 @@
 # Author  : MrFiona
 # File    : joseph_ring.py
 # Software: PyCharm Community Edition
+
+
+import time
+
+
+array_num_list = [num for num in range(1, 101)]
+print array_num_list
+
+k = 0
+m = 5
+temp = 0
+
+start = time.time()
+
+while sum(array_num_list):
+    k = temp
+    for ele in array_num_list:
+        if ele != 0:
+            k += 1
+            temp += 1
+        if k == m:
+            # time.sleep(1)
+            k = 0
+            temp = 0
+            array_num_list[array_num_list.index(ele)] = 0
+            print '编号为 %d 的人出局:\t', ele
+            # print 'array_num_list:\t', array_num_list
+
+print time.time() - start
