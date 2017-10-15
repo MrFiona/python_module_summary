f28232d28c4505d324b08bb02bda2a86f14e2a69
MrFiona <1160177283@qq.com>
2017-10-15 22:30:53 +0800

    ‘增加matplotlib

--- su_matplotlib/summary_matplotlib.py
+++ su_matplotlib/summary_matplotlib.py
@@ -3,13 +3,21 @@
 # Time    : 2017-10-15 11:17
 # Author  : MrFiona
 # File    : summary_matplotlib.py
-# Software: PyCharm Community Edition
+# Software: PyCharm Community Editionz`
 
 
-
+import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 
+# 通过rcParams设置全局横纵轴字体大小
+mpl.rcParams['xtick.labelsize'] = 24
+mpl.rcParams['ytick.labelsize'] = 24
 
 
+x = np.arange(0, 5, 0.1)
+y = np.sin(x)
+plt.plot(x, y)
 
 
+plt.show()
