c6863889c56bc8146b3b68f015d5a87f3e09b9e6
MrFiona <1160177283@qq.com>
2017-10-20 00:49:09 +0800

    更改文件summary_matplotlib.py

--- su_matplotlib/summary_matplotlib.py
+++ su_matplotlib/summary_matplotlib.py
@@ -11,13 +11,18 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 # 通过rcParams设置全局横纵轴字体大小
-mpl.rcParams['xtick.labelsize'] = 24
-mpl.rcParams['ytick.labelsize'] = 24
+# mpl.rcParams['xtick.labelsize'] = 24
+# mpl.rcParams['ytick.labelsize'] = 24
 
 
 x = np.arange(0, 5, 0.1)
 y = np.sin(x)
-plt.plot(x, y)
-
+print x
+print y
+# plt.plot(x, y)
+# plt.plot([1,2,3,4,5], np.arange(5, 10, 1),'r--', x,y, 'g-+')
+# plt.plot([1,2,3,4,5], 'r-+')
+plt.plot([1,2,3], [1,2,3], 'go-', label='line 1', linewidth=2)
+plt.plot([1,2,3], [1,4,9], 'rs',  label='line 2')
 
 plt.show()
---
+++ su_wxpython/reference_wxpython.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Time    : 2017-10-20 00:45
+# Author  : MrFiona
+# File    : reference_wxpython.py
+# Software: PyCharm Community Edition
