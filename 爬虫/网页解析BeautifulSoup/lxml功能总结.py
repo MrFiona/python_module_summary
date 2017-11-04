ef177a594cd0a3848a0cd8580e6d49ef71d5b72c
MrFiona <1160177283@qq.com>
2017-11-04 23:41:45 +0800

    增加lxml模块的总结

---
+++ 爬虫/网页解析BeautifulSoup/lxml功能总结.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Time    : 2017-11-04 23:33
+# Author  : MrFiona
+# File    : lxml功能总结.py
+# Software: PyCharm Community Edition
+
+
+import lxml
+
+
