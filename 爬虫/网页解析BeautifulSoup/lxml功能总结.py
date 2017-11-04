6d47f592255dbec61ccb8e9b539ad8c5b4f5321d
MrFiona <1160177283@qq.com>
2017-11-05 00:56:13 +0800

    更新lxmlzonj

--- 爬虫/网页解析BeautifulSoup/lxml功能总结.py
+++ 爬虫/网页解析BeautifulSoup/lxml功能总结.py
@@ -6,6 +6,34 @@
 # Software: PyCharm Community Edition
 
 
-import lxml
+from lxml import etree
+text = '''
+<div>
+    <ul>
+         <li class="item-0"><a href="link1.html">first item</a></li>
+         <li class="item-1"><a href="link2.html">second item</a></li>
+         <li class="item-inactive"><a href="link3.html">third item</a></li>
+         <li class="item-1"><a href="link4.html">fourth item</a></li>
+         <li class="item-0"><a href="link5.html">fifth item</a>
+     </ul>
+</div>
+'''
 
 
+html = etree.HTML(text)
+# html = etree.parse(text)
+result = etree.tostring(html)
+print(result)
+
+result = html.xpath('//li')
+print(result, type(result), len(result))
+
+for ele in result:
+    print(dir(ele))
+    print(ele.values)
+
+result = html.xpath('//li/@class')
+print(result, type(result), len(result))
+
+for ele in result:
+    print(ele)
