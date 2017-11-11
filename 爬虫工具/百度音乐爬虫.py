a41ecbbbff901bd53254352c750219f820620c05
MrFiona <1160177283@qq.com>
2017-11-12 03:46:38 +0800

    更新百度音乐爬虫

--- 爬虫工具/百度音乐爬虫.py
+++ 爬虫工具/百度音乐爬虫.py
@@ -7,13 +7,17 @@
 
 
 import re
-import base64
 import time
-import pandas as pd
+import json
 import requests
+from lxml import etree
 from bs4 import BeautifulSoup
 
 start_time = time.time()
+
+#todo 歌曲总分类标签以及链接信息
+global_music_classify_label_info = {}
+
 
 response = requests.get('http://music.baidu.com/tag')
 html = response.content.decode('utf-8')
@@ -21,66 +25,110 @@ html = response.content.decode('utf-8')
 htm_soup = BeautifulSoup(html, 'html.parser')
 classify_label_list = htm_soup.select('dl[class="tag-mod"]')
 # print(classify_label_list, len(classify_label_list))
+for element in classify_label_list:
+    label_name = element.find_all('dt')[0].string
+    classify_object_list = element.select('span[class="tag-list clearfix"]')
+    classify_music_link_info = { link.a.string : ''.join(['http://music.baidu.com', link.a.attrs['href']]) for link in classify_object_list }
+    # print('%s歌曲分类链接:\t' % label_name, classify_music_link_info)
+    global_music_classify_label_info[label_name] = classify_music_link_info
+
+# print(global_music_classify_label_info)
 
 try:
+    for classify in global_music_classify_label_info:
+        print(u'\033[32m开始下载总分类为 [{0}] 的歌曲\033[0m' .format(classify))
+        for child_classify in global_music_classify_label_info[classify]:
+            print(u'\033[33m开始下载子分类为 [{0}] 的歌曲\033[0m' .format(child_classify))
+            cycle_recursive_flag = True
+            cycle_url = global_music_classify_label_info[classify][child_classify]
+            page_num = 1
+            while cycle_recursive_flag:
+                print(u'开始爬取子分类 [{0}] 第 [{1}] 页的歌单' .format(child_classify, page_num))
+                time.sleep(4)
+                child_classify_response = requests.get(cycle_url)
+                child_classify_html = child_classify_response.content.decode('utf-8')
+                # print(child_classify_html)
+                #todo 歌曲分类标签
+                child_classify_soup = etree.HTML(child_classify_html)
+                label_a_list = child_classify_soup.xpath('//div[@class="main-body"]/div[@class="main-body-cont"]/div[@class="tag-main"]/div[@monkey="song-list"]/ul/li')
+                #todo 获取分类下的所有歌曲信息 会有50页歌单，每页20首歌曲
+                for music in label_a_list:
+                    music_info = music.attrib[u'data-songitem']
+                    json_music_info = json.loads(music_info)[u'songItem']
+                    print(json_music_info)
+                    #todo 歌曲名称
+                    music_name = json_music_info[u'author']
+                    print(music_name)
+                    #todo 歌手
+                    singer_name = json_music_info[u'sname']
+                    print(singer_name)
 
-    for classify in classify_label_list:
-        #todo 歌曲分类标签
-        # print(classify.dt)
-        # print(classify, type(classify))
-        label_a_list = classify.find_all('a')
-        #todo 歌曲分类标签下的子分类链接列表
-        classify_sep_url_list = [element.attrs['href'] for element in label_a_list]
-        label_sep_url_list = [element.string for element in label_a_list]
-        # print(label_sep_url_list)
-        # print()
+                    #todo 获取歌词，专辑信息
+                    #todo 获取链接
+                    other_info_link = ''.join([u'http://music.baidu.com/song/',json_music_info[u'sid']])
+                    print(other_info_link)
 
-        for sep_url in range(len(classify_sep_url_list)):
-            response = requests.get('http://music.baidu.com/{}' .format(classify_sep_url_list[sep_url]))
-            print(u'开始解析分类歌曲链接{}' .format(label_sep_url_list[sep_url]))
-            html = response.content.decode('utf-8')
-            # print(html)
+                    other_html = requests.get(other_info_link).content.decode('utf-8')
+                    other_soup = BeautifulSoup(other_html, 'html.parser')
+                    try:
+                        #todo 歌曲专辑
+                        album = other_soup.select('div[class="song-info"]')[0].select('div[class="song-info-box"]')[0].select('div[class="info-holder clearfix"]')[0].find_all('li', class_='clearfix')[0].a.stripped_strings
+                        album = list(album)[0]
+                        print(album)
+                    except:
+                        print(u'歌曲 [ {0} ] 获取专辑失败，获取专辑的链接为 [ {1} ]' .format(music_name, other_info_link))
 
-            label_sep_soup = BeautifulSoup(html, 'html.parser')
-            song_label_info = label_sep_soup.select('div[monkey="song-list"]')
-            song_label_list = song_label_info[0].select('li')
-            # print(song_label_list)
+                    #todo 获取歌词链接
+                    try:
+                        m = other_soup.select('div[id="lyricCont"]')[0].attrs['data-lrclink']
+                        #todo 获取歌词信息
+                        response = requests.get(m)
+                        html = response.content.decode('utf-8')
+                        for word in html.split('\n'):
+                            if re.search('^\[.*\]$', word):
+                                continue
+                            try:
+                                word = word.split(']')[1]
+                                print(word)
+                            except:
+                                pass
+                    except IndexError:
+                        print(u'歌曲 [ {0} ] 无歌词，获取歌词的链接为 [ {1} ]' .format(music_name, m))
 
-            for song in song_label_list:
-                # print(song)
-                #todo 歌曲详情子链接
-                song_sep_link = song.select('span[class="song-title"]')[0].select('a')[0].attrs['href']
-                #todo 歌曲作者，可能有多个作者
-                temp = song.select('span[class="singer"]')[0].select('a')
-                singer_list = [element.string for element in temp]
-                #todo 专辑名称
-                album_title = song.select('span[class="album-title"]')[0].select('a')[0].string
-                song_name = song.select('span[class="album-title"]')[0].select('a')[0].attrs['title']
+                    try:
+                        #todo 获取发行时间以及发行公司
+                        publish_sep_link = other_soup.select('div[class="song-info"]')[0].select('div[class="song-info-box"]')[0].select('div[class="info-holder clearfix"]')[0].find_all('li', class_='clearfix')[0].a['href']
+                        publish_info_link = ''.join([u'http://music.baidu.com', publish_sep_link])
+                        print(publish_info_link)
+                        publish_info_html = requests.get(publish_info_link).content.decode('utf-8')
+                        publish_info_html = publish_info_html.replace('&nbsp;', '')
+                        publish_soup = BeautifulSoup(publish_info_html, 'html.parser')
+                        publish_info = publish_soup.find_all('ul', class_="c6")[0].find_all('li')[-2].string
+                        #todo 发行时间
+                        print(publish_info)
+                        publish_info = re.split('\s{2,}', publish_info)
+                        #todo 以分号分隔,\uff1a为：的unicode编码
+                        publish_time = publish_info[0].split(u'\uff1a')[1].strip()
+                        #todo 发行公司
+                        publish_company = publish_info[1].split(u'\uff1a')[1].strip()
+                        print(publish_time)
+                        print(publish_company)
+                    except:
+                        print(u'歌曲 [ {0} ] 获取发行时间以及发行公司失败!!!，获取歌词的链接为 [ {1} ]' .format(music_name, publish_info_link))
 
-                print(u'歌曲子链接:\t', song_sep_link)
-                print(u'歌曲:\t', song_name)
-                print(u'歌手:\t', singer_list)
-                print(u'所属专辑:\t', album_title)
-                print()
+                #todo 获取下一页链接
+                try:
+                    next_page_link = child_classify_soup.xpath('//div[@class="page-cont"]/div[@class="page-inner"]/a[@class="page-navigator-next"]')[0].attrib[u'href']
+                    # print(dir(next_page_link))
+                    print next_page_link.strip()
+                    cycle_url = ''.join([u'http://music.baidu.com', next_page_link.strip()])
+                    print(cycle_url)
+                except:
+                    print(u'\033[35m爬取子分类为 [{0}]的歌曲结束，一共 [{1}] 页歌单\033[0m' .format(child_classify, page_num))
+                    cycle_recursive_flag = False
+                page_num += 1
 
-                # http://music.baidu.com/song/564102115
-                print(u'获取歌曲[ {} ]歌词开始.......' .format(song_name))
-                response = requests.get('http://music.baidu.com/{}' .format(song_sep_link))
-                html = response.content.decode('utf-8')
-
-                #todo 获取歌词链接
-                music_word_soup = BeautifulSoup(html, 'html.parser')
-                m = music_word_soup.select('div[id="lyricCont"]')[0].attrs['data-lrclink']
-                # print(m)
-                #todo 获取歌词信息
-                response = requests.get(m)
-                html = response.content.decode('utf-8')
-                # print(html.split('\n'))
-                for word in html.split('\n'):
-                    if re.search('^\[.*\]$', word):
-                        continue
-                    # word = word.split(']')
-                    # print(word)
-except ArithmeticError:
+except:
     print('error')
+
 print(time.time() - start_time)
